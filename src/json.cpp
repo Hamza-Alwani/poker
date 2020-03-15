d2b5ba27e9cf4de985e3a4992053e9d9f446ccd1
Hamza-Alwnai <hamzaalwani@gmail.com>
2020-03-15 18:24:57 -0500

    clean project

--- src/json.cpp
+++ src/json.cpp
@@ -3,18 +3,18 @@
 
 using json = nlohmann::json;
 
-	nlohmann::json create(json j, std::string n, int i, std::string t, std::string h[])
+	nlohmann::json create(json j, std::string n, int i, std::string t)//, std::string h[])
 	{
 		json k{
     
    		 	{"name",n},
     		{"id", i},
     		{"chrono",t},
-			{"card1",h[0]},
-    		{"card2",h[1]},
-    		{"card3",h[2]},
-    		{"card4",h[3]},
-    		{"card5",h[4]},
+//			{"card1",h[0]},
+ //   		{"card2",h[1]},
+  //  		{"card3",h[2]},
+   // 		{"card4",h[3]},
+    //		{"card5",h[4]},
     		
     	};	
     	return k;
