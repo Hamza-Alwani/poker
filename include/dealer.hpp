1e4c01624055413eaa29d6855b64e5d7d882937a
Hamza-Alwnai <hamzaalwani@gmail.com>
2020-03-17 14:17:44 -0500

    headers

---
+++ include/dealer.hpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+#include "asio.hpp"
+#include "json.hpp"
+class dealer
+{
+public:
+	void join()
+	{
+	
+	}
+	void leave()
+	{
+	
+	}
+	void send(nlohmann::json hand)
+	{
+	}
+	nlohmann::json deal()
+	{
+		nlohmann::json hand;
+		return hand;
+	}
+	nlohmann::json recive()
+	{
+		nlohmann::json hand;
+	}
+	int calculate_hand(nlohmann::json hand)
+	{
+		return int val;
+	}
+private:
+int numplayers;
+
+};
