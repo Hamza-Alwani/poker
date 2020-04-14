dc15b3cd3357496ded9b4115cf6276079c7dd29d
Hamza-Alwnai <hamzaalwani@gmail.com>
2020-04-14 18:31:50 -0500

    clean up

--- cleanup.sh
+++
@@ -1,6 +0,0 @@
-rm Makefile
-rm -rf CmakeFiles
-rm  *.glade
-rm cmake_install.cmake
-rm CMakeCache.txt
-rm -rf asio-1.12.2
--- run.sh
+++
@@ -1,6 +0,0 @@
-make clean
-make
-echo  "\n\n make done"
-#fuser -k 9000/tcp
-./src/gtk 127.0.0.1 9000
-echo  "\n\n run done"
